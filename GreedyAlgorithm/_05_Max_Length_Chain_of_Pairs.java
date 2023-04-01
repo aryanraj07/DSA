bd8c17ed7b31a4204e9980ad5b5ebcc0681fc31d
Aryan Raj <aryanraj0101010@gmail.com>
2023-04-01 18:44:08 +0530

    Chocola problem added

--- GreedyAlgorithm/_05_Max_Length_Chain_of_Pairs.java
+++ GreedyAlgorithm/_05_Max_Length_Chain_of_Pairs.java
@@ -3,7 +3,7 @@ package GreedyAlgorithm;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class _Max_Length_Chain_of_Pairs {
+public class _05_Max_Length_Chain_of_Pairs {
     public static void main(String[] args) {
         int [][]pairs={{5,24},{39,60},{5,28},{27,40},{50,90}};
         Arrays.sort(pairs,Comparator.comparingDouble(o->o[1]));
