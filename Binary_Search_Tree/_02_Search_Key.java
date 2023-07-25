cae327d076c0fb2846b7afaab7ad9e954eae204b
Aryan Raj <aryanraj0101010@gmail.com>
2023-07-25 13:37:59 +0530

    Search in a Binary Search Tree program added

---
+++ Binary_Search_Tree/_02_Search_Key.java
@@ -0,0 +1,65 @@
+package Binary_Search_Tree;
+
+public class _02_Search_Key {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data)
+        {
+            this.data=data;
+
+        }
+    }
+    public static Node insert(Node root, int val)
+    {
+        if(root==null)
+        {
+            root=new Node(val);
+            return root;
+        }
+        if(root.data>val)
+        {
+            root.left=insert(root.left,val);
+        }
+        else{
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
+    public static boolean search(Node root,int key)
+    {
+        if(root==null)
+        {
+            return false;
+        }
+        if(root.data==key)
+        {
+            return true;
+        }
+        if(root.data>key)
+        {
+            return search(root.left,key);
+        }
+        else
+        {
+            return  search(root.right,key);
+        }
+    }
+    public static void main(String[] args) {
+        int []values={5,1,3,4,2,7};
+        Node root=null;
+        for(int i=0;i<values.length;i++)
+        {
+            root=insert(root,values[i]);
+        }
+        if(search(root,1))
+        {
+            System.out.println("Found");
+        }
+        else{
+            System.out.println("Not found");
+        }
+
+    }
+}
