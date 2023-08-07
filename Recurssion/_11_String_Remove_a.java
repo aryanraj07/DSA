e41e7c9091ce22249637bfb22f41ee9d40fa1fb6
Aryan <aryanraj010010@gmail.com>
2023-08-07 20:47:21 +0530

    String remove another method program added

--- Recurssion/_11_String_Remove_a.java
+++ Recurssion/_11_String_Remove_a.java
@@ -1,25 +1,38 @@
 package Recurssion;
 
 public class _11_String_Remove_a {
-    public static String removeA(String str,int idx)
-    {
-        if(idx==str.length())
-        {
-            return  "";
+    public static String removeA(String str, int idx) {
+        if (idx == str.length()) {
+            return "";
         }
-        String smallString=removeA(str,idx+1);
-        char currChar=str.charAt(idx);
-        if(currChar!='a')
-        {
-            return currChar +smallString;
-        }
-        else{
+        String smallString = removeA(str, idx + 1);
+        char currChar = str.charAt(idx);
+        if (currChar != 'a') {
+            return currChar + smallString;
+        } else {
             return smallString;
         }
+
     }
-    public static void main(String[] args) {
-        String s="abdac";
-        System.out.println(s);
-        System.out.println(removeA(s,0));
+
+    public static String remove2(String s) {
+        if (s.length() == 0) {
+            return "";
+        }
+            String smallString = remove2(s.substring(1));
+            char currChar = s.charAt(0);
+            if (currChar != 'a') {
+                return currChar + smallString;
+            } else {
+                return smallString;
+            }
+        }
+
+        public static void main (String[]args){
+            String s = "abdac";
+            System.out.println(s);
+            System.out.println(removeA(s, 0));
+            System.out.println(remove2(s));
+        }
     }
-}
+
