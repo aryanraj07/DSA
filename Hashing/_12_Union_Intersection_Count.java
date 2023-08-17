a1d71aceea9e9a547a69c5175bdfc2be82c0ce39
Aryan <aryanraj010010@gmail.com>
2023-08-18 02:00:54 +0530

    Union and Intersection count  program added

---
+++ Hashing/_12_Union_Intersection_Count.java
@@ -0,0 +1,38 @@
+package Hashing;
+
+import java.util.HashSet;
+
+public class _12_Union_Intersection_Count {
+    public static void main(String[] args) {
+        int []arr1={7,3,9};
+        int []arr2={6,3,9,2,9,4};
+        HashSet<Integer> set=new HashSet<>();
+        //union
+        for(int i=0;i<arr1.length;i++)
+        {
+            set.add(arr1[i]);
+        }
+        for(int i=0;i<arr2.length;i++)
+        {
+            set.add(arr2[i]);
+        }
+        System.out.println("Count of union set is " +set.size());
+        set.clear();
+        //intersection
+        for(int i=0;i<arr1.length;i++)
+        {
+            set.add(arr1[i]);
+        }
+        int count=0;
+        for(int i=0;i<arr2.length;i++)
+        {
+            if(set.contains(arr2[i]))
+            {
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+        System.out.println("Size of intersection set is "+count);
+
+    }
+}
