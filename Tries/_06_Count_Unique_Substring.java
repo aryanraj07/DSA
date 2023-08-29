badd46e4b786074daa7379372c4d8d6309065359
Aryan <aryanraj010010@gmail.com>
2023-08-29 23:21:14 +0530

    Count unique Substring in  Trie program added

---
+++ Tries/_06_Count_Unique_Substring.java
@@ -0,0 +1,57 @@
+package Tries;
+
+public class _06_Count_Unique_Substring {
+    public static class Node{
+        Node children[]=new Node[26];
+        boolean eow;
+        Node()
+        {
+            for(int i=0;i<26;i++)
+            {
+                children[i]=null;
+
+            }
+
+        }
+    }
+    public static Node root=new Node();
+    public static void insert(String words)
+    {
+        Node curr=root;
+        for(int i=0;i<words.length();i++)
+        {
+            int idx=words.charAt(i)-'a';
+            if(curr.children[idx]==null)
+            {
+                curr.children[idx]=new Node();
+            }
+            curr=curr.children[idx];
+        }
+        curr.eow=true;
+    }
+    public static int countNodes(Node root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<26;i++)
+        {
+            if(root.children[i]!=null)
+            {
+                count+=countNodes(root.children[i]);
+            }
+        }
+            return count+1;
+    }
+    public static void main(String[] args) {
+String str="ababa";
+for(int i=0;i<str.length();i++)
+        {
+            String suffix=str.substring(i);
+            insert(suffix);
+        }
+        System.out.println(countNodes(root));
+    }
+}
